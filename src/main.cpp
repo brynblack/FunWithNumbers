1987f7b51a859501f5d6faddbee6fe8e90a185b7
brynblack <49110391+brynblack@users.noreply.github.com>
2022-03-04 19:25:09 +1100

    Implemented menu and tweaked compiler settings Added ability to receive input from user and determine which mode to select based upon choice. Also made library linking static to make builds portable.

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+// TODO: Add clear screen function
+// TODO: Implement different modes
+
+void checkNumberFeatures() {
+
+}
+
+void plotNumbers() {
+
+}
+
+void checkOverallStats() {
+
+}
+
+int main() {
+    char choice;
+    bool quit = false;
+
+    do {
+        std::cout << "Welcome to Fun With Numbers\n"
+                     "Choose from the menu below:\n"
+                     " (A) Check number features\n"
+                     " (B) Plot numbers\n"
+                     " (C) Check overall stats\n"
+                     "\n"
+                     " (X) Save and exit\n"
+                     "Choice: ";
+
+        std::cin >> choice;
+
+        switch (tolower(choice)) {
+            case 'a':
+                checkNumberFeatures();
+                break;
+            case 'b':
+                plotNumbers();
+                break;
+            case 'c':
+                checkOverallStats();
+                break;
+            case 'x':
+                quit = true;
+                break;
+            default:
+                break;
+        }
+    } while (!quit);
+
+    return 0;
+}
