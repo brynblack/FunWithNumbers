b99a76298884807559c305198674c6063af15651
brynblack <49110391+brynblack@users.noreply.github.com>
2022-03-07 07:40:21 +1100

    Implemented some code for mode 1

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,51 @@
 // TODO: Add clear screen function
 // TODO: Implement different modes
 
-void checkNumberFeatures() {}
+void checkNumberFeatures() {
+    std::string input;
+
+    std::cout << "Please enter a whole number that will be checked over: ";
+    std::getline(std::cin, input);
+    int number = std::stoi(input);
+
+    std::cout << "\n"
+                 "The features of " << number << " are...\n";
+
+    // REPLACE EVERY COMPONENT WITH A FUNCTION
+    // MAYBE ALSO USE CLASSES BECAUSE OBJECT ORIENTED PROGRAMMING
+    // Separate functions into separate source files
+    // e.g. isPositive(), isEven(), getFactors(), isPrime()
+    /* isPrime(2) ? cout << " true\n" : cout << " false\n";
+     * isPrime(156) ? cout << " true\n" : cout << " false\n";
+     */
+    if (number > 0) {
+        std::cout << " Positive\n";
+    }
+    else if (number < 0) {
+        std::cout << " Negative\n";
+    }
+    else {
+        std::cout << " Zero\n";
+    }
+    if (number % 2 == 0) {
+        std::cout << " Even\n";
+    }
+    else {
+        std::cout << " Odd\n";
+    }
+    std::cout << "Factors are ";
+    for (int i = 1; i <= number; i++) {
+        if (number % i == 0) {
+            std::cout << i << " ";
+        }
+    }
+    std::cout << "\n"; // Maybe use std::endl
+
+    // BUG: It crashes when the number is too large, or if a string is entered
+    // BUG: Factor determination crashes when number is too long or specific?
+
+    // Prime number?
+}
 
 void plotNumbers() {}
 
