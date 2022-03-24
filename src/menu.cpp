6898eab15750396c36e3faef3a585454ff771925
brynblack <49110391+brynblack@users.noreply.github.com>
2022-03-24 19:28:20 +1100

    Fixed #8, optimised code by a crap ton, and cleaned up code.

--- src/menu.cpp
+++ src/menu.cpp
@@ -3,34 +3,56 @@
 #include <iostream>
 
 namespace fwn {
-    void Menu::addOption(std::string key, std::function<void()> function, const std::string &description) {
+    // Adds a new option to the menu object.
+    void Menu::addOption(std::string key, std::function<void()> function, std::string description) {
+        // Converts the key to uppercase to allow for case insensitivity.
         std::transform(key.begin(), key.end(), key.begin(), ::toupper);
-        Option option { std::move(function), description };
+
+        // Instantiates an option object with the given function and description.
+        Option option { std::move(function), std::move(description) };
+
+        // Stores the option object in the menu object with the key as the location.
         this->choices[key] = option;
-        this->lines.push_back(description + "\n");
+
+        // Appends the description to the lines stored in the menu object.
+        this->lines.push_back(option.getDescription());
     }
 
+    // Adds a new line to the menu object.
     void Menu::addLine(const std::string &line) {
         this->lines.push_back(line);
     }
 
+    // Renders the main menu.
     void Menu::render() {
+        // Displays each line stored in the menu object.
         for (auto& line : this->lines) {
-            std::cout << line;
+            // Omit the newline if the line is the last line to be rendered.
+            if (&line == &this->lines.back()) {
+                std::cout << line;
+                continue;
+            }
+            std::cout << line << "\n";
         }
     }
 
+    // Executes the given option.
     void Menu::execute(std::string option) {
+        // Converts the option to uppercase for case insensitivity.
         std::transform(option.begin(), option.end(), option.begin(), ::toupper);
+
+        // Executes the option if it exists in the list of choices.
         if (choices.find(option) != choices.end()) {
             this->choices[option].execute();
         }
     }
 
+    // Executes the given function in the option object.
     void Menu::Option::execute() {
         function();
     }
 
+    // Retrieves the description of the option.
     std::string Menu::Option::getDescription() {
         return this->description;
     }
