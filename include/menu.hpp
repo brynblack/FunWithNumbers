ee4fbb368557c7f925c7eefb99ec08393c58262a
Brynley Llewellyn-Roux <blynblack@gmail.com>
2022-03-31 17:22:57 +1100

    Making CodeFactor happy

--- include/menu.hpp
+++ include/menu.hpp
@@ -9,32 +9,35 @@
 
 namespace fwn {
     class Menu {
-    private:
-        class Option;
-    public:
-		// Adds an option to a menu object.
-        void addOption(std::string key, std::function<void()> function, std::string description);
-		// Adds a line to a menu object.
-        void addLine();
-        void addLine(const std::string &line);
-		// Renders a menu.
-        void render();
-		// Executes a specified option stored in a menu object.
-        void execute(std::string option);
-    private:
-        std::map<std::string, Option> choices;
-        std::vector<std::string> lines;
-
-        class Option {
         private:
-            std::function<void()> function;
-            std::string description;
+            class Option;
+
         public:
-            Option() = default;
-            Option(std::function<void()> function, std::string description) : function(std::move(function)), description(std::move(description)) {};
-            void execute();
-            auto getDescription() -> std::string;
-        };
+            // Adds an option to a menu object.
+            void addOption(std::string key, std::function<void()> function, std::string description);
+            // Adds a line to a menu object.
+            void addLine();
+            void addLine(const std::string &line);
+            // Renders a menu.
+            void render();
+            // Executes a specified option stored in a menu object.
+            void execute(std::string option);
+
+        private:
+            std::map<std::string, Option> choices;
+            std::vector<std::string> lines;
+
+            class Option {
+                private:
+                    std::function<void()> function;
+                    std::string description;
+
+                public:
+                    Option() = default;
+                    Option(std::function<void()> function, std::string description) : function(std::move(function)), description(std::move(description)) {};
+                    void execute();
+                    auto getDescription() -> std::string;
+            };
     };
 } // namespace fwn
 
