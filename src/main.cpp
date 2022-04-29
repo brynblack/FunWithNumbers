603141915fd966958d3e3c5e5c8cbec467cf9306
Brynley Llewellyn-Roux <49110391+brynblack@users.noreply.github.com>
2022-04-29 14:08:18 +1000

    Added credits mode.

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,7 @@
 
 #include "fwn/modes/checknumberfeatures.hpp"
 #include "fwn/modes/checkoverallstats.hpp"
+#include "fwn/modes/credits.hpp"
 #include "fwn/modes/plotnumbers.hpp"
 
 #include <string>
@@ -43,6 +44,7 @@ auto mainMenu() -> void
     menu.add("a", "Check number features", fwn::modes::checkNumberFeatures);
     menu.add("b", "Plot numbers",          fwn::modes::plotNumbers);
     menu.add("c", "Check overall stats",   fwn::modes::checkOverallStats);
+    menu.add("g", "Credits",               fwn::modes::credits);
     menu.add();
     menu.add("x", "Save and exit",         [&quit]() -> void { quit = true; });
     menu.add("Choice: ");
